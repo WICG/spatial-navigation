bc8d112d30020f7f2cb54ccfba7fbe355b8fc669
Jihye Hong <jihyerish@gmail.com>
2019-02-22 00:57:55 +0900

    update

--- demo/virtual-scroller/scroller-utils.js
+++ demo/virtual-scroller/scroller-utils.js
@@ -1,6 +1,17 @@
-function addBoxes(root, amount) {
+function initBoxes(root, amount) {
   for (let i = 0; i < amount; i++) {
     root.innerHTML += `<div class= "item" tabindex="0" style="background-color: #${getRandomInt(100, 800)};"></div>`;
+  }
+}
+
+function addBoxes(root, amount) {
+  for (let i = 0; i < amount; i++) {
+    let temp = document.createElement('div');
+    temp.setAttribute('class', 'item');
+    temp.setAttribute('tabindex', 0);
+    temp.style.backGroundColor = `#${getRandomInt(100, 800)}`;
+
+    root.appendChild(temp);
   }
 }
 
@@ -8,19 +19,15 @@ function updateBoxes(root, amount) {
   const childNum = root.childElementCount;
 
   if (childNum > amount) {
-    console.log("start to remove");
-
     for (let i = 0; i < childNum - amount; i++) {
       root.removeChild(root.lastChild);
-
-      console.log("remove");
     }
   }
   else {
     for (let i = 0; i < amount - childNum; i++) {
       let temp = document.createNode('div');
       temp.setAttribute('class', 'item');
-      temp.setAttribute('tabinxed', 0);
+      temp.setAttribute('tabindex', 0);
       temp.style.backGroundColor = `#${getRandomInt(100, 800)}`;
 
       root.appendChild(temp);
