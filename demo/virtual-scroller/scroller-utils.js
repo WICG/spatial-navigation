24ee5bc009a8a22c04157338904222fbd364eec1
Jihye Hong <jh.hong@lge.com>
2019-02-25 10:03:37 +0900

    Update UI for the sample

--- demo/virtual-scroller/scroller-utils.js
+++ demo/virtual-scroller/scroller-utils.js
@@ -1,16 +1,8 @@
-function initBoxes(root, amount) {
-  for (let i = 0; i < amount; i++) {
-    root.innerHTML += `<div class= "item" tabindex="0" style="background-color: #${getRandomInt(100, 800)};"></div>`;
-  }
-}
-
 function addBoxes(root, amount) {
   for (let i = 0; i < amount; i++) {
     let temp = document.createElement('div');
     temp.setAttribute('class', 'item');
     temp.setAttribute('tabindex', 0);
-    temp.style.backGroundColor = `#${getRandomInt(100, 800)}`;
-
     root.appendChild(temp);
   }
 }
@@ -24,17 +16,6 @@ function updateBoxes(root, amount) {
     }
   }
   else {
-    for (let i = 0; i < amount - childNum; i++) {
-      let temp = document.createNode('div');
-      temp.setAttribute('class', 'item');
-      temp.setAttribute('tabindex', 0);
-      temp.style.backGroundColor = `#${getRandomInt(100, 800)}`;
-
-      root.appendChild(temp);
-    }
+    addBoxes(root, amount);
   }
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
 }
