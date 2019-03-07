261905b2442a44e6649544198fe7ec786bc9f84f
jeonghee27 <jeonghee27.ahn@lge.com>
2019-03-07 18:48:52 +0900

    Add internal TC for polyfill.

---
+++ tests/internal/test.js
@@ -0,0 +1,49 @@
+let resultIndex = 1;
+
+function testInit() {
+  const resultNode = document.createElement('div');
+  resultNode.id = 'result';
+  document.body.appendChild(resultNode);
+}
+
+function testRun(func, description) {
+  try {
+    func();
+    addResult ('SUCCESS', description);
+  } catch (error) {
+    addResult ('FAIL', description, error);
+  }
+}
+
+function addResult (success, description, error) {
+  const resultNode = document.querySelector('#result');
+  const testResult = document.createElement('div');
+  const testDes1 = document.createElement('span');
+  const testDes2 = document.createElement('span');
+  const testDes3 = document.createElement('span');
+  testDes1.innerText = resultIndex++ + '.' + success;
+  testDes2.innerText = description;
+
+  testResult.appendChild(testDes1);
+  testResult.appendChild(testDes2);
+  if (error) {
+    testDes1.className = 'fail';
+    testDes3.innerText = error;
+    testDes3.className = 'error';
+    testResult.appendChild(testDes3);
+  }
+  resultNode.appendChild(testResult);
+}
+
+function getString (obj) {
+  if(!obj || typeof obj !== "object")
+    return obj;
+  else if ("nodeType" in obj && "nodeName" in obj && "nodeValue" in obj && "childNodes" in obj)
+    return obj.outerHTML.substr(0,60) + '...';
+  return obj.toString();
+}
+
+function assert_equals(actual, expected) {
+  if (actual !== expected)
+    throw `assert_equals: expected ${getString(expected)} but got  ${getString(actual)}`;
+}
